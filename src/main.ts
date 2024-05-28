ff67133f07ff7cd25db1c9b0098d736ce630c09f
zicozydailva <zicozydasilva70@gmail.com>
2024-05-28 23:34:36 +0100

    feat: spin auth-svc microservice & setup kafka

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,46 @@
 import { NestFactory } from '@nestjs/core';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './filters';
+import { LoggerInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan('dev'));
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new LoggerInterceptor());
+
+  const KAFKA_BROKER = app.get(ConfigService).get('KAFKA_BROKER');
+
+  const microservice = app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [KAFKA_BROKER],
+      },
+      consumer: {
+        groupId: 'AUTH_MS',
+      },
+    },
+  });
+
+  // await app.listen(3000);
+  // global config
+  app.setGlobalPrefix('/api');
+  await app.listen(configService.get<string>('PORT'));
+
+  await microservice.listen();
 }
 bootstrap();
